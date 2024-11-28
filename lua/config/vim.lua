c64b0b124d6689d9beb7dfe5eb9568a903bb335b
Elouan Martinet <exa@elou.world>
2024-11-28 12:53:03 +0100

    WIP [skip-ci] [skip ci]

--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -9,6 +9,7 @@ vim.opt.list = true
 vim.opt.listchars = {tab = "  ", trail = " ", nbsp = " ", lead = "·"}
 vim.opt.scrolloff = 2
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 vim.g.mapleader = ","
 
