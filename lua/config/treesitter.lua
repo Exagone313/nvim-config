75b091685a936c2c17775cf2374ed95309a5ae36
Elouan Martinet <exa@elou.world>
2026-04-16 01:26:21 +0200

    WIP [skip-ci] [skip ci]

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -2,20 +2,20 @@
 -- SPDX-FileCopyrightText: 2024 Elouan Martinet <exa@elou.world>
 -- SPDX-License-Identifier: BSD-3-Clause
 
-require("nvim-treesitter.configs").setup{
-	ensure_installed = {
-		"bash",
-		"diff",
-		"git_rebase",
-		"gitcommit",
-		"markdown",
-		"markdown_inline",
-		"python",
-	},
-	highlight = {
-		enable = true,
-	},
-	indent = {
-		enable = true,
-	},
+require('nvim-treesitter').install {
+	"bash",
+	"diff",
+	"git_rebase",
+	"gitcommit",
+	"markdown",
+	"markdown_inline",
+	"python",
 }
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'bash', 'sh', 'diff', 'gitrebase', 'gitcommit', 'markdown', 'python' },
+	callback = function()
+		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
+})
