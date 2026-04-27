f9544c571e08da07f84c2417bbad12f2a93060e8
Elouan Martinet <exa@elou.world>
2026-04-27 15:41:09 +0000

    WIP [skip-ci] [skip ci]

--- lua/config/above.lua
+++ lua/config/above.lua
@@ -24,12 +24,29 @@ require("which-key").add({
 	menu.open,
 	desc = "Leader menu",
 })
+
+
+local function tabs_previewer_ctor()
+	local builtin = require("fzf-lua.previewer.builtin")
+	local TabsPreviewer = builtin.buffer_or_file:extend()
+	function TabsPreviewer:parse_entry(entry_str, _cb)
+		if type(entry_str) == "string" and entry_str:match("^[^\t]*\t%d+\t%d+\t0%)") then
+			return { content = {} }
+		end
+		return TabsPreviewer.super.parse_entry(self, entry_str, _cb)
+	end
+	return TabsPreviewer
+end
+
 require("fzf-lua").setup({
 	tabs = {
 		keymap = {
 			fzf = {
 				["focus"] = [[transform:[ {4} = 0 ] && case "$FZF_ACTION" in *up) echo up ;; *) echo down ;; esac]],
 			},
+		},
+		previewer = {
+			_ctor = tabs_previewer_ctor,
 		},
 	},
 })
