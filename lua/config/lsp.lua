eff4d56b847d0cdafa0e34dee582c75839119ce7
Elouan Martinet <exa@elou.world>
2026-08-15 08:04:35 +0200

    improve: use Mini Icons in Blink Completion

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -82,6 +82,28 @@ require("blink-cmp").setup{
 		keyword = {
 			range = 'full',
 		},
+		menu = {
+			draw = {
+				components = {
+					kind_icon = {
+						text = function(ctx)
+							local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+							return kind_icon
+						end,
+						highlight = function(ctx)
+							local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+							return hl
+						end,
+					},
+					kind = {
+						highlight = function(ctx)
+							local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+							return hl
+						end,
+					}
+				}
+			}
+		}
 	},
 	keymap = {
 		preset = 'none',
